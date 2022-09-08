3ebe5d28fbfd6e40384f6496af40903f83733789
Sujith-9392666154 <112929829+Sujith-9392666154@users.noreply.github.com>
2022-09-08 09:59:12 +0530

    Add files via upload

---
+++ user name(valid or not).py
@@ -0,0 +1,9 @@
+name1 = input('enter the user name: ')
+name2 = input('re enter the user name: ')
+if (name1 == name2):
+    print('the user name is valid')
+    
+else:
+    print('the user name is invalid')
+        
+
